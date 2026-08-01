9d92dd1793043d091a1de9582b6f8861bae1f296
mansiaer <ns.nana77777@gmail.com>
2026-08-01 18:52:46 +0000

    Backport Integrate hook KSU
    # Conflicts:
    #	fs/exec.c
    #	fs/open.c
    #	fs/read_write.c
    #	kernel/sys.c

--- fs/read_write.c
+++ fs/read_write.c
@@ -596,12 +596,18 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 	return ret;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_sys_read(unsigned int fd,
+                                char __user **buf_ptr, size_t *count_ptr);
+#endif
 SYSCALL_DEFINE3(write, unsigned int, fd, const char __user *, buf,
 		size_t, count)
 {
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
-
+#ifdef CONFIG_KSU
+                ksu_handle_sys_read(fd, &buf, &count);
+#endif
 	if (f.file) {
 		loff_t pos = file_pos_read(f.file);
 		ret = vfs_write(f.file, buf, count, &pos);
