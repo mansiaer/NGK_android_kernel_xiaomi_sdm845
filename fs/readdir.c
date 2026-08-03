7654f8150447fad84cb9b239c875bd1c1b213647
mansiaer <ns.nana77777@gmail.com>
2026-08-03 18:02:41 +0000

    Integrate NoMount

--- fs/readdir.c
+++ fs/readdir.c
@@ -21,6 +21,10 @@
 
 #include <asm/uaccess.h>
 
+#ifdef CONFIG_NOMOUNT
+extern int nomount_handle_iterate_dir(struct file *file, struct dir_context *ctx);
+#endif
+
 int iterate_dir(struct file *file, struct dir_context *ctx)
 {
 	struct inode *inode = file_inode(file);
@@ -46,11 +50,15 @@ int iterate_dir(struct file *file, struct dir_context *ctx)
 	res = -ENOENT;
 	if (!IS_DEADDIR(inode)) {
 		ctx->pos = file->f_pos;
+#ifdef CONFIG_NOMOUNT
+               res = nomount_handle_iterate_dir(file, ctx);
+#else
 		ctx->romnt = (inode->i_sb->s_flags & MS_RDONLY);
 		if (shared)
 			res = file->f_op->iterate_shared(file, ctx);
 		else
 			res = file->f_op->iterate(file, ctx);
+#endif
 		file->f_pos = ctx->pos;
 		fsnotify_access(file);
 		file_accessed(file);
